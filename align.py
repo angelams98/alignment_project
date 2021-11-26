08e2d08b1924b498c8a9398af2ce37176781bc47
angelams98 <angelams98@gmail.com>
2021-11-26 14:39:07 +0100

    align.py updated

--- align.py
+++ align.py
@@ -5,51 +5,19 @@ import sys
 
 
 #PSEUDOCODE
-    #Call read_files function to get a list with the sequences we are going to align
-    #(seq, title) = read_files("dna7.fsa")
-
-    #Choose 2 sequences and call alignment_simple function to create a list of list with the scores
-    #To call this function you have to add the 2 sequences and the values for match, mismatch and Indel
-    #possible values for match, mismatch and indel:
-    #1, -1, -1
-    #0, 1, 1
-    #0, 1, 10
-    # matrix = alignment_simple(seq[0], seq[1], 1, -1, -1)
-
-    #If you want to print this matrix on screen, call print_matrix function
-    #print_matrix(matrix)
-
-    #To calculate the best alignment using the scores from the matrix, call traceback matrix
-    #As inputs for the function you need the matrix, and the original strings
-    #print(traceback(matrix, seq[0], seq[1]))
-
-
-#The program can get several arguments: file, match, mismatch, indel, extension.
-#The first argument is the name of the file
-#The second, third, fourth and fifth value are integers
-
-#List of links that contain different blosum matrix
-#https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
-#http://www.cbcb.umd.edu/confcour/CMSC423-materials/BLOSUM80.txt
-#https://github.com/noporpoise/seq-align/blob/master/scoring/BLOSUM50.txt
 
 accepted_filetypes = ['.fsa', '.fasta', '.fna']
 
 #Read the file
 if len(sys.argv) == 2:
-    try:
-        infile = sys.argv[1]
-
-    except IOError:
-        print("The file you introduce is not accessible")
-        sys.exit(1)
-
+    infile = sys.argv[1]
 else:
     infile = input("Give the name of the infile: ")
 
 
 
 # Check that the specified file is a fasta file
+#O(m), m is the number of elements in accepted_filetypes
 if re.search(r'\.\w+$', infile).group(0) in accepted_filetypes:
 
     #Try to open the file and generates an error message if it fails
@@ -58,7 +26,7 @@ if re.search(r'\.\w+$', infile).group(0) in accepted_filetypes:
         infile = open(infile, 'r')
 
     except IOError as err:
-        print("can't open file, reason:", str(err))
+        print("Can't open file, reason:", str(err))
         sys.exit(1)
 
 else:
@@ -73,19 +41,24 @@ sequences = ""
 nucleotides = ["A", "T", "G", "C"]
 amino_acids = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", 
                 "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
-amino_acids_checker = ["D", "E", "F", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "V", "W", "Y"]
+amino_acids_checker = ["D", "E", "F", "H", "I", "K", "L", "M", "N", 
+                        "P", "Q", "R", "S", "V", "W", "Y"]
+blosum_files = ["30", "35", "40", "45", "50", "55", "62", "65", "70", 
+                "75", "80", "85", "90", "100"]
 is_protein = False
 pos = 0
 
 
-#Saves the titles and the sequences in lists
-#O(m), m is the number of lines in the file
+#Saves the header lines and the sequences in lists
+#O(n), n is the number of lines in the file
 for line in infile:
     if line.startswith('>'):
+        #O(1), it is appending an element
         title.append(line[:-1])
 
         #If in the next line after ">", sequences is not empty, it is added to seq_list
         if sequences != "":
+            #O(1), it is appending an element
             seq_list.append(sequences)
         sequences = ""
 
@@ -93,21 +66,23 @@ for line in infile:
         sequences += line[:-1] 
 
 if sequences != "":
+    #O(1), it is appending an element
     seq_list.append(sequences)
 
 
-#Create an only string with the sequence
+#Create an only string with the sequences
 seq = "".join(seq_list)
 
 
-# Check if sequence from infile is either a DNA or a protein sequence
-#O(n), n is the number of sequences (theoretically only 2)
+#Check if sequence from infile is either a DNA or a protein sequence
+#O(o), o is the number of sequences (theoretically only 2)
 while pos < len(seq):
     
+    #O(p), p is the number of elements in amino_acid_checker list
     if seq[pos] in amino_acids_checker:
         is_protein = True
 
-    #O(p), p is the number of elements in amino_acids list
+    #O(q), q is the number of elements in amino_acids list
     elif seq[pos] not in amino_acids:
         print("The sequence found in the file", infile.name, "contained an impure sequence.")
         sys.exit(1)
@@ -119,25 +94,28 @@ while pos < len(seq):
 
 #They are DNA sequences
 if is_protein == False:
+    
+    dna_prot = "dna"
+    #This variable is not used in DNA
+    blosum = 0
 
     print("What you have given me is a DNA sequence \n")
     alignment = input("So do you want to do local or global alignment?\n")
     alignment = alignment.upper()
-    dna_prot = "dna"
-    blosum = 0
+    print("Please tell me which parameters you want to use\n")
 
-    #O(q), q is the strings we are comparing in the conditional
+    match = float(input("Give me the match value: "))
+    mismatch = float(input("Give me the mismatch value: "))
+    opening = float(input("Give me the indel value: "))
+    extension = float(input("Give me the extension value: "))
+    print("\n")
+
+
+    #O(r), r is the strings we are comparing in the conditional
     if alignment == "GLOBAL":
 
         print("Perfect, we are going to do a local alignment of your sequence, we will be applying \nNeedleman-Wunshman method (nice guy by the way)\n")
-        print("Please tell me which parameters you want to use\n")
-
-        match = int(input("Give me the match value: "))
-        mismatch = int(input("Give me the mismatch value: "))
-        opening = int(input("Give me the indel value: "))
-        extension = int(input("Give me the extension value: "))
-        print("\n")
-
+        print("And the parameters are match:{}, mismatch:{}, opening score:{} and extension score:{}\n".format(match, mismatch, opening, extension))
         
         (matrix, alignment) = alignment_nw(seq_list[0], seq_list[1], dna_prot, match, mismatch, opening, extension)
         print("Needleman-Wunsch alignment for:\n{}\n{}\n".format(title[0], title[1]))
@@ -147,87 +125,67 @@ if is_protein == False:
     elif alignment == "LOCAL":
 
         print("Perfect, we are going to do a global alignment of your sequence, we will be applying \nSmith-Waterman method (nice guy by the way)\n")
-        print("Please tell me which parameters you want to use\n")
-
-        match = int(input("Give me the match value: "))
-        mismatch = int(input("Give me the mismatch value: "))
-        opening = int(input("Give me the indel value: "))
-        extension = int(input("Give me the extension value: "))
-        print("\n")
-
-
+        print("And the parameters are match:{}, mismatch:{}, opening score:{} and extension score:{}\n".format(match, mismatch, opening, extension))
 
         (matrix, alignment, score) = alignment_sw(seq_list[0], seq_list[1], dna_prot, match, mismatch, opening, extension)
 
         print("Smith-Waterman alignment for:\n{}\n{}\n".format(title[0], title[1]))
         print(alignment)
         print("The score of the best alignment is", score[0])
-
-        
+    
+    else:
+        print("I cannot recognize this alignment")
+        sys.exit(1)
 
 
 #They are protein sequences
 elif is_protein == True:
 
+    dna_prot = "protein"
+    #Those variables are not used in proteins
+    match = 0 
+    mismatch = 0
+    
     print("What you have given me is a protein sequence \n")
     alignment = input("So do you want to do local or global alignment?\n")
     alignment = alignment.upper()
-    dna_prot = "protein"
-    match = 0 
-    mismatch = 0
 
-    #O(q), q is the strings we are comparing in the conditional
+    print("Please tell me which parameters you want to use\n")
+    opening = float(input("Give me the indel value: "))
+    extension = float(input("Give me the extension value: "))
+    blosum_int = input("Give me which blosum matrix you want to use (introduce the number only): ")
+    print("\n")
+
+    #O(s), s is the number of the elements in the blosum_files list
+    if blosum_int in blosum_files:
+        blosum = blosum_int 
+
+    #O(s), s is the number of the elements in the blosum_files list
+    if blosum_int not in blosum_files:
+        print("I don't have that file, I am using BLOSUM62 instead\n")
+        blosum = "62"
+
+
+
     if alignment == "GLOBAL" :
 
         print("Perfect, we are going to do a local alignment of your sequence, we will be applying \nNeedleman-Wunshman method (nice guy by the way)\n")
-        print("Please tell me which parameters you want to use\n")
+        print("And the parameters are opening score:{}, extension score:{} and blosum_matrix\n".format(opening, extension, blosum))
 
-        opening = int(input("Give me the indel value: "))
-        extension = int(input("Give me the extension value: "))
-        blosum_int = input("Give me which blosum matrix you want to use (introduce the number only):")
-        print("\n")
-
-        if blosum_int == "62":
-            blosum = blosum_matrix("BLOSUM62.txt")
-        elif blosum_int == "80":
-            blosum = blosum_matrix("BLOSUM80.txt")
-        elif blosum_int == "50":
-            blosum = blosum_matrix("BLOSUM50.txt")
-        else:
-            print("I don't have that file, I am using BLOSUM62 instead\n")
-            blosum = blosum_matrix("BLOSUM62.txt")
-
-        
-        (matrix, alignment) = alignment_nw(seq_list[0], seq_list[1], dna_prot, opening, extension, match, mismatch)
         print("Needleman-Wunsch alignment for:\n{}\n{}\n".format(title[0], title[1]))
-        print(alignment)
+        print(alignment_nw(seq_list[0], seq_list[1], dna_prot, opening, extension, match, mismatch))
         
 
-    #O(q), q is the strings we are comparing in the conditional
     elif alignment == "LOCAL":
 
         print("Perfect, we are going to do a global alignment of your sequence, we will be applying \nSmith-Waterman method (nice guy by the way)\n")
-        print("Please tell me which parameters you want to use\n")
-
-        opening = int(input("Give me the indel value: "))
-        extension = int(input("Give me the extension value: "))
-        blosum_int = input("Give me which blosum matrix you want to use (introduce the number only): ")
-        print("\n")
-
-
-        if blosum_int == "62":
-            blosum = blosum_matrix("BLOSUM62.txt")
-        elif blosum_int == "80":
-            blosum = blosum_matrix("BLOSUM80.txt")
-        elif blosum_int == "50":
-            blosum = blosum_matrix("BLOSUM50.txt")
-        else:
-            print("I don't have that file, I am using BLOSUM62 instead\n")
-            blosum = blosum_matrix("BLOSUM62.txt")
-
-    
+        print("And the parameters are opening score:{}, extension score:{} and blosum_matrix\n".format(opening, extension, blosum))
+        
         (matrix, alignment, score) = alignment_sw(seq_list[0], seq_list[1], dna_prot, opening, extension, match, mismatch)
-
         print("Smith-Waterman alignment for:\n{}\n{}\n".format(title[0], title[1]))
         print(alignment)
         print("The score of the best alignment is", score[0])
+
+    else:
+        print("I cannot recognise the alignment\n")
+        sys.exit(1)
